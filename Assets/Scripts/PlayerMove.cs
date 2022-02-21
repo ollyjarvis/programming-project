a405b3fff3a9e80ee33cd2e18a0b47091c69e2ec
ollyjarvis <olly.m.jarvis@gmail.com>
2022-02-21 14:21:36 +0000

    Added meterial and changed movement slightly

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -3,8 +3,10 @@ using UnityEngine;
 public class PlayerMove : MonoBehaviour
 {
     [SerializeField]
-    private float speed = 3f; // Float for the speed of the plyyer, private so that it cannot be changed by that player
+    private float speed = 2.3f; // Float for the speed of the player, private so that it cannot be changed by that player
     // Serialized so that it appears in the Unity editor to make it easier to edit during testing
+    [SerializeField]
+    private float sprintSpeed = 3f;
 
     public CharacterController controller; // Character controller called controller, used to apply the movement to the player
     public Vector3 velocity = Vector3.zero;  //  Create a 3D vector with magnitude and direction 0
@@ -12,7 +14,7 @@ public class PlayerMove : MonoBehaviour
     public float gravity = -9.81f; // Value of gravity, used when player jumps to make them fall back to the ground
 
     [SerializeField]
-    public float jumpHeight = 2f; // Arbitrary number used in the gravity calcuations as a scalar to increase or decrease height of jumps
+    public float jumpHeight = 1.3f; // Arbitrary number used in the gravity calcuations as a scalar to increase or decrease height of jumps
 
     public Transform groundCheck; // Empty object that sits at the players feet which is used as the position for the ground check
     public float groundDistance = 0.4f; // The radius of the sphere that checks if the player is on the ground
@@ -44,11 +46,18 @@ public class PlayerMove : MonoBehaviour
         Vector3 moveVertical = transform.forward * zMovement;  //  Create 3D vector for forward/ backward movement equal to the
         //  vector transform.forward (0, 0, 1) mulptiplied by the users forward input
 
-        Vector3 velocityNorm = (moveHorizontal + moveVertical).normalized * speed; //  Add the x and z vectors and normalize it to
+        Vector3 velocityNorm = (controller.velocity + moveHorizontal + moveVertical).normalized; //  Add the x and z vectors and normalize it to
         // make the vectors magnitude equal to 1 so that the player does not move faster when walking diagonally and multiply by speed
 
-        controller.Move(velocityNorm * speed * Time.deltaTime); // Uses move function to move the character controller at the speed set
-        // Uses Time.deltaTime to make the movement frame independant else players with more powerful machines would move faster
+        if(Input.GetButton("Shift"))
+        {
+            controller.Move(velocityNorm * speed * Time.deltaTime); // If the player is holding shift the player will sprint
+        }
+        else
+        {
+            controller.Move(velocityNorm * sprintSpeed * Time.deltaTime); // Uses move function to move the character controller at the speed set
+            // Uses Time.deltaTime to make the movement frame independant else players with more powerful machines would move faster
+        }
 
         if(Input.GetButtonDown("Jump") && isGrounded) // If the player presses jump and the player is on the floor
         {
